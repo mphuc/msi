3c2b977bc56b7ee6278d5ed6c9fb271bfee70619
Your Name <mphuc2790@gmail.com>
2017-04-22 18:35:21 +0700

    a

--- catalog/model/account/pd.php
+++ catalog/model/account/pd.php
@@ -120,13 +120,6 @@ class ModelAccountPd extends Model {
 	}
 
 	public function saveInvoice($customer_id,$invoice_id_hash, $redeem_code, $amount, $amount_usd,$type,$my_wallet){
-		$date_added= date('Y-m-d H:i:s');
-		$date_added = strtotime ( '+ 7 hour' , strtotime ($date_added));
-		$date_added= date('Y-m-d H:i:s',$date_finish) ;
-
-		$date_finish= date('Y-m-d H:i:s');
-		$date_finish = strtotime ( '+ 7.50 hour' , strtotime ($date_added));
-		$date_finish= date('Y-m-d H:i:s',$date_finish) ;
 		$query = $this -> db -> query("
 			INSERT INTO ".DB_PREFIX."customer_invoice_pd SET
 			customer_id = '".$customer_id."',
@@ -138,8 +131,8 @@ class ModelAccountPd extends Model {
 			type = '".$type."',
 			input_address = '".$my_wallet."',
 			my_address = '".$my_wallet."',
-			date_created = '".$date_added."',
-			date_finish = '".$date_finish."'
+			date_created = NOW(),
+			date_finish = DATE_ADD(NOW(),INTERVAL + 30 minute)
 		");
 
 		return $query === True ? $this->db->getLastId() : -1;
